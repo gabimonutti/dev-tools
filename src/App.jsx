9bfacd483828dbbf9996eda49337b598008d1208
gabimonutti <gabimonutti1@gmail.com>
2023-12-20 00:00:58 -0300

    Fix problem with going back to home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,21 @@ import './App.css'
 import { Route, Router, Switch } from 'wouter';
 import Home from './components/Home';
 import ToolDetail from './components/ToolDetail';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const handlePopstate = () => {
+      window.location.reload();
+    };
+
+    window.addEventListener('popstate', handlePopstate);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopstate);
+    };
+  }, []);
+
   return (
     <Router>
         <Switch>
